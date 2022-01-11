0d7b17953eb77d2770988a7c7b171dc020a5de25
nghiemthodo1998@gmail.com <nghiemthodo1998@gmail.com>
2022-01-11 23:37:04 +0700

    update redux in DetailProduct.js

--- src/Pages/DetailProduct/DetailProduct.js
+++ src/Pages/DetailProduct/DetailProduct.js
@@ -1,25 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import TopNav from '../../Components/TopNav/TopNav';
 import './DetailProduct.css';
 import Picture from '../../Picture/Image.png';
 import { Button, Rate } from 'antd';
 import { useSelector } from 'react-redux';
-import axios from 'axios';
+
 const DetailProduct = () => {
   const { id } = useParams();
-
   const [detailProduct, setDetailProduct] = useState({});
-
-  useEffect(() => {
-    axios.get(`http://localhost:8000/products/${id}`).then((res) => {
-      setDetailProduct(res.data);
-    });
-  }, [id]);
 
   const products = useSelector((state) => state.product.products);
 
-  console.log(detailProduct);
+  useEffect(() => {
+    const getProductById = async () => {
+      await products.forEach((product) => {
+        if (product.id === +id) {
+          setDetailProduct(product);
+        }
+      });
+    };
+    getProductById();
+  }, [id, products]);
 
   const handleClick = () => {
     //
